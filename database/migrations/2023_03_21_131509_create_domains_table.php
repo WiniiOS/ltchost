851ec2da8514a7336a753b4cf6a2f9ad974aa868
garaa2n <franckndi5@gmail.com>
2023-03-21 14:36:58 +0100

    init project

---
+++ database/migrations/2023_03_21_131509_create_domains_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('domains', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('userId');
+            $table->string('domainName');
+            $table->string('dns1');
+            $table->string('dns2');
+            $table->date('expirationDate');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('domains');
+    }
+};
